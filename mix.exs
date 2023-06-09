6d377f61159c6eadd117c84ad9cab4c4424abe1a
Billal GHILAS <bilal@evercam.io>
2023-06-09 15:16:33 +0100

    fix github url

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Membrane.RTP.H265.Mixfile do
   use Mix.Project
 
   @version "0.1.0"
-  @github_url "https://github.com/membraneframework/membrane_rtp_h265_plugin"
+  @github_url "https://github.com/gBillal/membrane_rtp_h265_plugin"
 
   def project do
     [

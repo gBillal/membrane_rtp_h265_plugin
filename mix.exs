8cf198912af611b5a429160b196616cc1869537e
Billal GHILAS <bilal@evercam.io>
2023-08-06 22:35:50 +0100

    Release v0.3.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.RTP.H265.Mixfile do
   use Mix.Project
 
-  @version "0.2.0"
+  @version "0.3.0"
   @github_url "https://github.com/gBillal/membrane_rtp_h265_plugin"
 
   def project do
@@ -36,8 +36,8 @@ defmodule Membrane.RTP.H265.Mixfile do
 
   defp deps do
     [
-      {:membrane_core, "~> 0.11.0"},
-      {:membrane_rtp_format, "~> 0.6.0"},
+      {:membrane_core, "~> 0.12.0"},
+      {:membrane_rtp_format, "~> 0.7.0"},
       {:membrane_h265_format, "~> 0.1.0"},
       # Dev
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
